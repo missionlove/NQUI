ccea93f1643696f0f073b3902d70d5a8da6c1932
missionlove <31380043+missionlove@users.noreply.github.com>
2026-01-26 11:50:43 +0800

    Add Demo1 and NFramelessWidget Qt project files
    Initial commit of Demo1 and NFramelessWidget sample projects, including source, header, and Qt project files. This sets up the basic structure for demos and plugins in the NQUI repository.

---
+++ NQUI/NPlugins/NFramelessWidget/nframelesswidget.cpp
@@ -0,0 +1,5 @@
+#include "nframelesswidget.h"
+
+NFramelessWidget::NFramelessWidget()
+{
+}
---
+++ NQUI/NPlugins/NFramelessWidget/nframelesswidget.h
@@ -0,0 +1,12 @@
+#ifndef NFRAMELESSWIDGET_H
+#define NFRAMELESSWIDGET_H
+
+#include "NFramelessWidget_global.h"
+
+class NFRAMELESSWIDGET_EXPORT NFramelessWidget
+{
+public:
+    NFramelessWidget();
+};
+
+#endif // NFRAMELESSWIDGET_H
